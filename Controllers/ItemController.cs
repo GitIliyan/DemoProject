a2867ccd881904d05c27356929bf24d078530369
GitIliyan <69449892+GitIliyan@users.noreply.github.com>
2021-09-02 10:11:09 +0300

    Add files via upload

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml.Office.CustomUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ItemController : Controller
+    {
+        //database entity 
+
+        public ItemController()
+        {
+
+        }
+        // GET: Item
+        public ActionResult Index()
+        {
+            IDs objIDs = new IDs();
+            
+            
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(IDs objIDs)
+        {
+            return Json(data:"HHH", JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ Models/IDs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Models
+{
+    public class IDs
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public DateTime DeliveryOn { get; set; }
+        public IEnumerable<SelectListItem> SelectListItems { get; set; }
+    }
+}
